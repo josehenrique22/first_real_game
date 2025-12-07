9d3a9390ef1aa181c5b7336a1793fbdfd8ec26a2
joser <joserbtgt@gmail.com>
2025-12-07 06:21:16 -0300

    movement dto

---
+++ scripts/entities/player/dto/MovementDTO.cs
@@ -0,0 +1,9 @@
+using Godot;
+
+namespace DTOS;
+public partial class MovementDTO : Node
+{
+    public float Speed {get; set;}
+    public float Acceleration {get; set;}
+    public float Deceleration {get; set;}
+}
