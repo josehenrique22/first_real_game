e90f1ecdbd51ab1b601d38f51c86c7ef5f5637fa
joser <joserbtgt@gmail.com>
2025-12-09 09:48:57 -0300

    clean code debbug mesh line

--- scenes/generic_system_scenes/DebugMeshLine.cs
+++ scenes/generic_system_scenes/DebugMeshLine.cs
@@ -5,47 +5,53 @@ public partial class DebugMeshLine : MeshInstance3D
 
     [Export] CharacterBody3D _meshPosition;
 
-    public override void _Process(double delta)
+    private enum MeshLineMagniture : int
     {
-        GenerateLine(Vector3.Zero, Vector3.Forward * 3, Colors.Red);
+        LineLenght = 5
     }
 
-    private void GenerateLine(Vector3 _pointA, Vector3 _pointB, Color _meshColor)
+    public override void _Process(double delta)
     {
-        if (_pointA.IsEqualApprox(_pointB)) { return; }
+        GenerateLine();
+    }
 
-        if (Mesh is ImmediateMesh newMeshLine)
+    private void GenerateLine()
+    {
+        DrawMeshLine(Vector3.Zero,
+        Vector3.Forward *
+        (int)MeshLineMagniture.LineLenght,
+        Colors.Red);
+        SetMeshLinePosition();
+    }
+
+    private void SetMeshLinePosition()
+    {
+        Position = _meshPosition.Position;
+        if (!MeshConnectToEntity())
         {
-            // Clear the buffer before start drawing
-            newMeshLine.ClearSurfaces();
-
-            // Start drawing
-            newMeshLine.SurfaceBegin(Mesh.PrimitiveType.Lines);
-
-            // Set Color and Cordinates
-            newMeshLine.SurfaceAddVertex(_pointA);
-
-            newMeshLine.SurfaceAddVertex(_pointB);
-
-            newMeshLine.SurfaceSetColor(_meshColor);
-
-            var lineDebugPosition = MeshSamePositionToPlayer() ?
-                "Position is equal to player" : "Position is not equal to the player";
-
-            Position = _meshPosition.Position;
-            if (!MeshSamePositionToPlayer())
-            {
-                GlobalPosition = _meshPosition.GlobalPosition;
-            }
-
-            GD.Print(lineDebugPosition);
-
-            // End Drawing
-            newMeshLine.SurfaceEnd();
+            GlobalPosition = _meshPosition.GlobalPosition;
         }
     }
 
-    private bool MeshSamePositionToPlayer()
+    private void DrawMeshLine(Vector3 _pointA, Vector3 _pointB, Color _meshColor)
+    {
+        if (_pointA.IsEqualApprox(_pointB)) { return; }
+
+        var newMeshLine = Mesh as ImmediateMesh;
+
+        newMeshLine.ClearSurfaces();
+
+        newMeshLine.SurfaceBegin(Mesh.PrimitiveType.Lines);
+
+        newMeshLine.SurfaceAddVertex(_pointA);
+        newMeshLine.SurfaceAddVertex(_pointB);
+
+        newMeshLine.SurfaceSetColor(_meshColor);
+
+        newMeshLine.SurfaceEnd();
+    }
+
+    private bool MeshConnectToEntity()
     {
         return Position == _meshPosition.Position;
     }
